32ae166d660a50201a2304b021e3945103cd3e1a
danailvidev <danailvidev@gmail.com>
2018-02-12 17:30:24 +0200

    typo

--- frontend/src/app/features/github/github.service.ts
+++ frontend/src/app/features/github/github.service.ts
@@ -8,13 +8,12 @@ import { GithubApi, Issue } from './interfaces';
 export class GithubService {
     baseUrl: string = environment.github.baseUrl;
     repoName: string = environment.github.repoName;
-    issueUrl: string = environment.github.IssueUrl;
+    issueUrl: string = environment.github.issueUrl;
 
     public constructor(protected http: HttpClient) {
     }
 
     getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
-        console.log(sort, order, page )
         const requestUrl =
             `${this.baseUrl}?q=repo:${this.repoName}&sort=${sort}&order=${order}&page=${page + 1}`;
         return this.http.get<GithubApi>(requestUrl);
