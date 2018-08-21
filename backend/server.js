066aac76f0a0a6416d860d3ae081e334d16f34d0
Danail Videv <danailvidev@gmail.com>
2018-08-21 14:13:35 +0300

    ssl

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
+// var fs = require('fs');
+// var privateKey  = fs.readFileSync('./key.txt', 'utf8');
+// var certificate = fs.readFileSync('./crt.txt', 'utf8');
+// var credentials = {key: privateKey, cert: certificate};
 var express = require('express')
 var app = express()
 const config = require('./config/config')
-var server = require('http').createServer(app)
+var httpServer = require('http').createServer(app)
+// var httpsServer = require('https').createServer(credentials, app)
 var api = require('./middleware/routes')
 
 // middleware 
@@ -14,6 +19,7 @@ require('./middleware/database')()
 app.use('/api', api)
 
 // socket.io
-require('./middleware/socketio')(server)
+require('./middleware/socketio')(httpServer)
 
-server.listen(config.port || 3000)
+httpServer.listen(config.port || 3000)
+// httpsServer.listen(8443)
