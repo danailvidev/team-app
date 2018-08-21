a6db19af396a3e77f24b4bbc752c61867d72f561
Danail Videv <danailvidev@gmail.com>
2018-08-21 13:47:59 +0300

    update nodemon

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -2,11 +2,11 @@ var cors = require('cors')
 var bodyParser = require('body-parser')
 const config = require('../config/config')
 
-module.exports = function(app){
+module.exports = function (app) {
     app.use(cors({
         credentials: true,
         origin: config.cors == undefined ? 'http://localhost:4200' : config.cors.headers
     }));
-    
+
     app.use(bodyParser.json())
 }
