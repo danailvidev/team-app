67cb1b9882b3ecb3e68db737230eccb5142712e7
Danail Videv <danailvidev@gmail.com>
2018-10-13 17:34:28 +0300

    update packages

--- frontend/src/app/features/calendar/calendar.module.ts
+++ frontend/src/app/features/calendar/calendar.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { SharedModule } from '../../shared/shared.module';
-import { CalendarModule as ngCalendar } from 'angular-calendar';
+import { CalendarModule as ngCalendar, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   imports: [
     SharedModule,
     CalendarRoutingModule,
-    ngCalendar.forRoot(),
+    ngCalendar.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory
+      }),
 
   ],
   declarations: [CalendarRoutingModule.components]
