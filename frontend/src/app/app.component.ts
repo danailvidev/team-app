dc61290b918a0d69675e7d9ca53d68bc0e268232
Danail Videv <danailvidev@gmail.com>
2018-11-07 17:58:42 +0200

    mobile menu, media q

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,34 +1,47 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { AuthService } from './core/auth.service';
 import { Router } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     navigation = [
         { link: 'payment', label: 'Payment', color: '', icon: 'payment' },
         { link: 'github', label: 'GitHub Issues', color: '', icon: 'code' },
         { link: 'team', label: 'Team Service', color: '', icon: 'share' },
         { link: 'users', label: 'Users', color: '', icon: 'people' },
-        { link: 'calendar', label: 'Calendar', color: '' , icon: 'calendar_today'}
+        { link: 'calendar', label: 'Calendar', color: '', icon: 'calendar_today' }
     ];
     theme = 'light-theme';
 
     @HostBinding('class') componentCssClass;
 
+    mobileQuery: MediaQueryList;
+    private mobileQueryListener: () => void;
+
     constructor(
         public auth: AuthService,
         private router: Router,
-        public overlayContainer: OverlayContainer) {
+        public overlayContainer: OverlayContainer,
+        changeDetectorRef: ChangeDetectorRef,
+        media: MediaMatcher) {
+        this.mobileQuery = media.matchMedia('(max-width: 800px)');
+        this.mobileQueryListener = () => changeDetectorRef.detectChanges();
 
     }
 
     ngOnInit() {
         this.setTheme(this.theme);
+        this.mobileQuery.addListener(this.mobileQueryListener);
+    }
+
+    ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this.mobileQueryListener);
     }
 
     setTheme(theme) {
